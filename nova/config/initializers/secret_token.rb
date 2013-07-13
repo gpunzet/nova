bad63348a63f60d7f69f450447c576c9c6e4c2f9
alrod894 <alberto_894@hotmail.com>
2013-07-13 11:28:32 -0500

    algo

---
+++ nova/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nova::Application.config.secret_key_base = '93624867ed255aea7ea2668c3214c4089b32d934b7722a81596a6f4cf7448051d627419602b3b60f98def78084984254d993ba6dce968cfdff382dd43c646668'
